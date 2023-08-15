eec17b53646d38e97f8f2035b364f7c2554146fc
kaseyclary <mail.kaseyc@gmail.com>
2023-08-15 18:16:56 -0500

    added viewport restrictions

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,7 @@ export default function App({ Component, pageProps }) {
     <SessionProvider session={pageProps.session}>
       <UserProvider>
         <WorkoutProvider>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
           <BottomNav>
             <Component {...pageProps}/>
             <ToastContainer />
