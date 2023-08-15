e7b2884b55a699e75f8d9ef58ce89f481ddb0014
kaseyclary <mail.kaseyc@gmail.com>
2023-08-15 15:32:25 -0500

    added border to final like pic

--- components/WorkoutCard/SocialSection.jsx
+++ components/WorkoutCard/SocialSection.jsx
@@ -29,14 +29,14 @@ export default function SocialSection ({workout, onLikeUpdate}) {
             <div className="w-5/6 mx-auto flex items-center pb-3">
                 {workout.likes.length === 1 ? (
                     <div className="flex items-center">
-                        <img src ={workout.likes[0].userPhoto} className="h-[27px] w-[27px] rounded-full mr-1"/>
+                        <img src ={workout.likes[0].userPhoto} className="h-[27px] w-[27px] border-[2px] border-white rounded-full mr-1"/>
                         <p className="text-xs font-semibold">Knucks from {workout.likes[0].userName}</p>
                     </div>
                 ) : workout.likes.length === 2 ? (
                     <div className="flex items-center">
                         <div className="flex">
                             <img src ={workout.likes[0].userPhoto} className="h-[27px] w-[27px] border-[2px] border-white rounded-full drop-shadow"/>
-                            <img src={workout.likes[1].userPhoto} className="h-[27px] w-[27px] -ml-3 rounded-full mr-1"/>
+                            <img src={workout.likes[1].userPhoto} className="h-[27px] w-[27px] border-[2px] border-white -ml-3 rounded-full mr-1"/>
                         </div>
                         <p className="text-xs font-semibold">Knucks from {workout.likes[0].userName} and {workout.likes.length - 1} other</p>
                     </div>
@@ -45,7 +45,7 @@ export default function SocialSection ({workout, onLikeUpdate}) {
                         <div className="flex">
                             <img src ={workout.likes[0].userPhoto} className="h-[27px] w-[27px] border-[2px] border-white rounded-full drop-shadow"/>
                             <img src ={workout.likes[0].userPhoto} className="h-[27px] w-[27px] border-[2px] border-white rounded-full drop-shadow"/>
-                            <img src={workout.likes[1].userPhoto} className="h-[27px] w-[27px] -ml-3 rounded-full mr-1"/>
+                            <img src={workout.likes[1].userPhoto} className="h-[27px] w-[27px] border-[2px] border-white -ml-3 rounded-full mr-1"/>
                         </div>
                         <p className="text-xs font-semibold">Knucks from {workout.likes[0].userName} and {workout.likes.length - 1} others</p>
                     </div>
