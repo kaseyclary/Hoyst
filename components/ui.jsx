0b6a46b93ae0dccfdef7c8f0878da0add834c594
kaseyclary <mail.kaseyc@gmail.com>
2023-08-21 16:34:59 -0500

    fixed knucks display issue

--- components/ui.jsx
+++ components/ui.jsx
@@ -25,12 +25,12 @@ export const WorkoutLikes = ({ workout }) => {
             </div>
             <p className="text-xs font-semibold">Knucks from {workout.likes[0].userName} and {workout.likes.length - 1} other</p>
         </div>
-    ) : workout.likes.length === 2 ? (
+    ) : workout.likes.length > 2 ? (
         <div className="flex items-center" onClick={() => router.push(`/workouts/${workout._id}/knucks`)}>
             <div className="flex">
-                <img src ={workout.likes[0].userPhoto} className="h-[27px] w-[27px] border-[2px] border-white rounded-full drop-shadow"/>
-                <img src ={workout.likes[0].userPhoto} className="h-[27px] w-[27px] border-[2px] border-white rounded-full drop-shadow"/>
-                <img src={workout.likes[1].userPhoto} className="h-[27px] w-[27px] border-[2px] border-white -ml-3 rounded-full mr-1"/>
+                <img src ={workout.likes[0].userPhoto} className="h-[27px] w-[27px] border-[2px] border-white rounded-full drop-shadow z-20"/>
+                <img src ={workout.likes[1].userPhoto} className="h-[27px] w-[27px] border-[2px] border-white -ml-3 rounded-full drop-shadow"/>
+                <img src={workout.likes[2].userPhoto} className="h-[27px] w-[27px] border-[2px] border-white -ml-3 rounded-full mr-1"/>
             </div>
             <p className="text-xs font-semibold">Knucks from {workout.likes[0].userName} and {workout.likes.length - 1} others</p>
         </div>
