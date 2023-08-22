df9e6851044cf6b71e79f2f78e6652d33b5a7e79
kaseyclary <mail.kaseyc@gmail.com>
2023-08-21 21:03:52 -0500

    profile update - added activity feature

--- pages/Home.js
+++ pages/Home.js
@@ -21,29 +21,33 @@ export default function Home({ workouts }) {
   : orderedWorkouts;
 
   return (
-    <div className="min-h-screen bg-slate-100 pt-[50px] max-w-screen overflow-x-hidden">
-      <div className="flex w-full px-2 py-2 ">
-        <Select 
-          placeholder="Filter..." 
-          className="w-full" 
-          options={liftTypes}
-          value={selectedLift ? {label: selectedLift, value: selectedLift} : null}
-          onChange={(option) => setSelectedLift(option ? option.label : null)}
-        />
-        <button className={selectedLift ? " px-3 py-1 bg-red-600 ml-2 rounded text-sm text-white font-medium" : "hidden"} onClick={() => setSelectedLift(null)}>Clear</button>
+    <div className="bg-slate-100">
+      <div className="min-h-screen bg-slate-100 pt-[50px] max-w-[600px] mx-auto">
+        <div className="">
+          <div className="flex w-full px-2 py-2 sticky top-0">
+            <Select 
+              placeholder="Filter..." 
+              className="w-full" 
+              options={liftTypes}
+              value={selectedLift ? {label: selectedLift, value: selectedLift} : null}
+              onChange={(option) => setSelectedLift(option ? option.label : null)}
+            />
+            <button className={selectedLift ? " px-3 py-1 bg-red-600 ml-2 rounded text-sm text-white font-medium" : "hidden"} onClick={() => setSelectedLift(null)}>Clear</button>
+          </div>
+        </div>
+          {filteredWorkouts.length ? (
+          <div className="max-w-[600px] mx-auto pb-20">
+            {filteredWorkouts.map((workout) => (
+                <WorkoutCard key={workout._id} workout={workout} />
+            ))}
+          </div>
+        ) : (
+          <div className="max-w-[600px] mx-auto pb-20 px-4 pt-12 h-screen flex flex-col justify-center text-slate-800">
+            <h3 className="text-[1.5rem] font-semibold mb-4">No workouts to display yet.</h3>
+            <p className="text-[1.2rem] font-medium">Either <Link className="underline text-orange-600" href="/AddWorkout">add a workout</Link> or <Link className="underline text-orange-600" href="/friends">follow some friends</Link> to start growing your feed!</p>
+          </div>
+        )}
       </div>
-        {filteredWorkouts.length ? (
-        <div className="max-w-[600px] mx-auto pb-20">
-          {filteredWorkouts.map((workout) => (
-              <WorkoutCard key={workout._id} workout={workout} />
-          ))}
-        </div>
-      ) : (
-        <div className="max-w-[600px] mx-auto pb-20 px-4 pt-12 h-screen flex flex-col justify-center text-slate-800">
-          <h3 className="text-[1.5rem] font-semibold mb-4">No workouts to display yet.</h3>
-          <p className="text-[1.2rem] font-medium">Either <Link className="underline text-orange-600" href="/AddWorkout">add a workout</Link> or <Link className="underline text-orange-600" href="/friends">follow some friends</Link> to start growing your feed!</p>
-        </div>
-      )}
     </div>
   ) 
 }
--- pages/profile.js
+++ pages/profile.js
@@ -2,12 +2,20 @@ import { getSession } from 'next-auth/react';
 import clientPromise from '../lib/db'; // Assuming you have a db client setup
 import { findBestEffort } from '@/lib/utils';
 import { useState, useEffect } from 'react';
+import { liftTypes } from '@/lib/utils';
+import Select from 'react-select';
+import WorkoutCard from '@/components/WorkoutCard/WorkoutCard';
+import Link from 'next/link';
 
 export default function Profile({ user, workouts }) {
+
+    const options = ["Activity", "Featured", "Progress"]
 
     const [bestSquat, setBestSquat] = useState(0);
     const [bestBench, setBestBench] = useState(0);
     const [bestDeadlift, setBestDeadlift] = useState(0);
+    const [selectedLift, setSelectedLift] = useState(null);
+    const [selectedOption, setSelectedOption] = useState(options[0]);
 
     useEffect(() => {
         if (!workouts) return;
@@ -16,31 +24,96 @@ export default function Profile({ user, workouts }) {
         setBestDeadlift(findBestEffort(workouts, "Deadlift"));
     }, [workouts])
 
+    const orderedWorkouts = workouts.sort((a, b) => {
+        return new Date(b.date) - new Date(a.date);
+    });
+
+    const filteredWorkouts = selectedLift
+    ? orderedWorkouts.filter(workout => 
+        workout.lifts.some(lift => lift.name === selectedLift)
+      )
+    : orderedWorkouts;
+
+    function ActivityView() {
+        return (
+            <div>
+                <div className="relative">
+                    <div className="flex w-full px-2 py-2 sticky top-0">
+                    <Select 
+                        placeholder="Filter..." 
+                        className="w-full" 
+                        options={liftTypes}
+                        value={selectedLift ? {label: selectedLift, value: selectedLift} : null}
+                        onChange={(option) => setSelectedLift(option ? option.label : null)}
+                    />
+                    <button className={selectedLift ? "px-3 py-1 bg-red-600 ml-2 rounded text-sm text-white font-medium" : "hidden"} onClick={() => setSelectedLift(null)}>Clear</button>
+                    </div>
+                    <div className="">
+                        {selectedLift && filteredWorkouts.length === 1 && (
+                            <div className="p-4">
+                                <p className="text-sm font-medium">{user.firstName} has {filteredWorkouts.length} {selectedLift} Workout recorded</p>
+                                <p className="text-sm font-semibold">His best effort is {findBestEffort(filteredWorkouts, selectedLift)} lbs</p>
+                            </div>
+                        )}
+                        {selectedLift && filteredWorkouts.length > 1 && (
+                            <div className="p-4">
+                                <p className="text-sm font-medium">{user.firstName} has {filteredWorkouts.length} {selectedLift} Workouts recorded</p>
+                                <p className="text-sm font-semibold">His best effort is {findBestEffort(filteredWorkouts, selectedLift)} lbs</p>
+                            </div>
+                        )}
+                        
+                    </div>
+                </div>
+                {filteredWorkouts.length ? (
+                    <div className="max-w-[600px] mx-auto pb-20">
+                    {filteredWorkouts.map((workout) => (
+                        <WorkoutCard key={workout._id} workout={workout} />
+                    ))}
+                    </div>
+                ) : (
+                    <div className="max-w-[600px] mx-auto pb-20 px-4 pt-12 h-screen flex flex-col justify-center text-slate-800">
+                    <h3 className="text-[1.5rem] font-semibold mb-4">No workouts to display yet.</h3>
+                    <p className="text-[1.2rem] font-medium"><Link className="underline text-orange-600" href="/AddWorkout">Add a workout</Link> to start growing your profile activity!</p>
+                    </div>
+                )}
+            </div>
+        )
+    }
+
     return (
-        <div className="mt-[100px] max-w-[600px] mx-auto px-4 w-screen">
-            <div className="flex items-center mb-6">
+        <div className="mt-[80px] max-w-[600px] mx-auto w-screen text-slate-700">
+            <div className="flex items-center mb-6 px-4">
                 <div className="flex items-center mr-3">
                     <img className="w-[60px] h-[60px] rounded-full" src={user.photoUrl} alt="Profile Picture" />
                 </div>
                 <div className="flex flex-col justify-center">
-                    <h2 className="text-[2rem] font-bold">{user.firstName} {user.lastName}</h2>
-                    <h3>{user.email}</h3>
+                    <h2 className="text-[1.25rem] font-bold">{user.firstName} {user.lastName}</h2>
+                    <div className="flex text-sm font-medium">
+                        <img src="/barbell.png" alt="Barbell" className="w-[17px] h-[17px] mr-1" />
+                        <p className="text-sm">{user.workouts ? user.workouts.length : "0"} recorded workouts</p>
+                    </div>
+                    <div className="flex w-full mb-3 text-sm font-medium">
+                        <p>Following: {user.following ? user.following.length : "0"}</p>
+                        <p className="mx-2">|</p>
+                        <p> Followers: {user.followers ? user.followers.length : "0"}</p>
+                    </div>
                 </div>
             </div>
-            <div className="flex w-full mb-3 text-sm font-medium">
-                <p>Following: {user.following ? user.following.length : "0"}</p>
-                <p className="mx-2">|</p>
-                <p> Followers: {user.followers ? user.followers.length : "0"}</p>
-            </div>
-            <div className="flex text-sm font-medium">
-                <img src="/barbell.png" alt="Barbell" className="w-[17px] h-[17px] mr-2" />
-                <p className="text-sm">{user.workouts ? user.workouts.length : "0"} recorded workouts</p>
+            <div className="grid grid-cols-3 font-semibold text-sm [&>*]:border-r [&>*]:py-[10px] [&>*:last-child]:border-0">
+                <div className={selectedOption === options[0] ? "selected-profile-option" : "profile-option"} onClick={() => setSelectedOption(options[0])}>
+                    <p className="">{options[0]}</p>
+                </div>
+                <div className={selectedOption === options[1] ? "selected-profile-option" : "profile-option"} onClick={() => setSelectedOption(options[1])}>
+                    <p className="">{options[1]}</p>
+                </div>
+                <div className={selectedOption === options[2] ? "selected-profile-option" : "profile-option"} onClick={() => setSelectedOption(options[2])}>
+                    <p className="">{options[2]}</p>
+                </div>
             </div>
             <div>
-                <h3 className="text-lg font-bold mt-6 mb-3">Big 3: {bestSquat + bestBench + bestDeadlift} Total.</h3>
-                <p>Squat Max: {bestSquat}</p>
-                <p>Bench Max: {bestBench}</p>
-                <p>Deadlift Max: {bestDeadlift}</p>
+                {selectedOption === options[0] && (
+                    <ActivityView />
+                )}
             </div>
         </div>
     )
@@ -77,8 +150,9 @@ export async function getServerSideProps(context) {
     const workoutCollection = db.collection('workouts');
     let workouts = [];
     let bestLifts = [];
+    let workoutIds = [];
     if(user.workouts && user.workouts.length > 0) {
-        const workouts = await workoutCollection.find({ _id: { $in: user.workouts } }).toArray();
+        workouts = await workoutCollection.find({ _id: { $in: user.workouts } }).toArray();
     }
 
     return {
